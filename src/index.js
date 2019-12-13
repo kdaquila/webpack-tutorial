f89b25204ea5125bf7d4ae1eb23d0c6c390b4c93
Kenneth D'Aquila <kfd182@gmail.com>
2019-12-12 23:18:39 -0600

    Asset Management tutorial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import _ from 'lodash';
+import css from './style.css';
+import scss from './mystyle.scss';
+import logo from './logo.svg';
 
 function component() {
-    const element = document.createElement('div');
+    const divElement = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //Add the image
+    const myLogo = new Image();
+    myLogo.src = logo;
+    divElement.appendChild(myLogo);
 
-    return element;
+    // Add the heading
+    const headingElement = document.createElement('span');
+    headingElement.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    headingElement.classList.add(scss.hello);
+    headingElement.classList.add(css.hello);
+    divElement.appendChild(headingElement);
+
+    return divElement;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,31 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', {loader: 'css-loader', options: {modules: true}}]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    {loader: 'css-loader', options: {modules: true}},
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
+
+        ]
+    }
 };
